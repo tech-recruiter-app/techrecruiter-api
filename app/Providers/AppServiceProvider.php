13467f6605097929ce35868b3726b9d1cd043b8d
Yvan Tchuente <yvantchuente@gmail.com>
2026-01-21 15:38:46 -0500

    Fix code quality issues from PHP version bump

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,12 +6,14 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Override;
 
 final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
+    #[Override]
     public function register(): void
     {
         //
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Override;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -15,6 +16,7 @@ abstract class TestCase extends BaseTestCase
      */
     protected $seed = true;
 
+    #[Override]
     protected function setUp(): void
     {
         parent::setUp();
