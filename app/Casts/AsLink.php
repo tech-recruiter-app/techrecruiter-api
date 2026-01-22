98b879e682c4977405a387febaff5c3677b3d7ad
Yvan Tchuente <yvantchuente@gmail.com>
2026-01-21 23:52:28 -0500

    Add custom cast for the link value object
    Provide a custom cast to transform web link values to the link value object.

---
+++ app/Casts/AsLink.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Casts;
+
+use App\Values\Link;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
+use LogicException;
+use ReflectionProperty;
+
+/**
+ * @implements CastsAttributes<Link, Link>
+ */
+final class AsLink implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): ?Link
+    {
+        if ($value === null) {
+            return null;
+        }
+        if (! is_string($value)) {
+            throw new InvalidArgumentException("The $key value must be a string.");
+        }
+
+        $link = new Link($value);
+        new ReflectionProperty(Link::class, 'exists')->setValue($link, true);
+
+        return $link;
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): ?string
+    {
+        if ($value === null) {
+            return null;
+        }
+        // @phpstan-ignore instanceof.alwaysTrue
+        if (! $value instanceof Link) {
+            throw new InvalidArgumentException("The $key value given must be of type ".Link::class);
+        }
+
+        if (! $value->exists) {
+            throw new LogicException('The link must exist in order to be saved.');
+        }
+
+        return (string) $value;
+    }
+}
