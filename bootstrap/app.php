e8749dc5b90cc284316211303cab6859b6317115
Yvan Tchuente <yvantchuente@gmail.com>
2026-04-27 20:28:18 -0400

    Standardize HTTP response rendition of exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use App\Exceptions\Renderer;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +20,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(fn (Throwable $e, Request $request): JsonResponse => new Renderer()->render($e, $request));
     })->create();
